b41ef1b0a8acf186c46d0ac8132ff7edec600553
Sandeep26071994 <103501540+Sandeep26071994@users.noreply.github.com>
2022-05-01 17:09:45 +0530

    Create app.py

---
+++ app/server/app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from app.server.routes.user import router as userRouter
+
+app = FastAPI()
+
+app.include_router(userRouter, tags=["user"], prefix="/user")
+
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Welcome to this fantastic app!"}
